04169aea8eff3063f9a22cff59f2aa6fc82a2d6a
Linus Nilsson <linus.nilsson@trimma.se>
2020-12-03 20:00:03 +0100

    Day 3

---
+++ Day03/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+var lines = File.ReadAllLines("day03.txt");
+
+int CountTreesInSlope(int right, int down)
+{
+	int x = 0, y = 0;
+	int hits = 0;
+	while (y + down < lines.Length)
+	{
+		x = (x + right) % lines[0].Length;
+		y += down;
+		if (lines[y][x] == '#') hits++;
+	}
+
+	return hits;
+}
+
+void Part1()
+{
+	Console.WriteLine($"Part 1: {CountTreesInSlope(3, 1)}");
+}
+
+void Part2()
+{
+	var hits = CountTreesInSlope(1, 1)
+		* CountTreesInSlope(3, 1)
+		* CountTreesInSlope(5, 1)
+		* CountTreesInSlope(7, 1)
+		* CountTreesInSlope(1, 2);
+
+	Console.WriteLine($"Part 2: {hits}");
+}
+
+Part1();
+Part2();
