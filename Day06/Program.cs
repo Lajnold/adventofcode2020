17ba8dcbb4f0a821c18d915e2b320e8613047ebe
Linus Nilsson <linus.nilsson@trimma.se>
2020-12-06 12:46:41 +0100

    Day 6

---
+++ Day06/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var groups = File.ReadAllText("day06.txt")
+	.Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+	.Select(g => g.Split("\n", StringSplitOptions.RemoveEmptyEntries))
+	.ToList();
+
+void Part1()
+{
+	var sum = 0;
+	foreach (var group in groups)
+	{
+		sum += group.SelectMany(g => g).Distinct().Count();
+	}
+	Console.WriteLine($"Part 1: {sum}");
+}
+
+void Part2()
+{
+	var sum = 0;
+	foreach (var group in groups)
+	{
+		sum += group.SelectMany(g => g).Distinct().Count(c => group.All(g => g.Contains(c)));
+	}
+	Console.WriteLine($"Part 2: {sum}");
+}
+
+Part1();
+Part2();
