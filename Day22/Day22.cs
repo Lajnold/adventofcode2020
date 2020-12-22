4d69f64d96bd0acf8507311619a56f4622b5d648
Linus Nilsson <121645+lmonilsson@users.noreply.github.com>
2020-12-22 19:52:27 +0100

    Day 22, part 1

---
+++ Day22/Day22.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+Part1();
+
+void Part1()
+{
+    var decks = LoadPlayerDecks();
+
+    while (decks.All(d => d.Any()))
+    {
+        var topCards = decks.Select(d => d[0]).ToList();
+        var maxPlayer = topCards.Select((c, i) => (c, i)).OrderByDescending(ci => ci.c).First().i;
+        foreach (var deck in decks)
+        {
+            deck.RemoveAt(0);
+        }
+        decks[maxPlayer].AddRange(topCards.OrderByDescending(c => c));
+    }
+
+    var winnerDeck = decks.First(d => d.Any());
+    var winnerScore = winnerDeck.Reverse<int>().Select((c, i) => c * (i + 1)).Sum();
+    Console.WriteLine($"Part 1: {winnerScore}");
+}
+
+List<int>[] LoadPlayerDecks()
+{
+    var lines = File.ReadLines("day22.txt");
+    var deck1 = lines.Skip(1).TakeWhile(x => x != "").Select(int.Parse).ToList();
+    var deck2 = lines.SkipWhile(x => x != "Player 2:").Skip(1).Select(int.Parse).ToList();
+    return new [] { deck1, deck2 };
+}
